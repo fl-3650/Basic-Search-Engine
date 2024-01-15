c29be4f9aae0709e746de555cfa45de77d9dc2c6
Ivan Spacebiker <97333947+fl-3650@users.noreply.github.com>
2024-01-15 18:14:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,29 @@
+#include <algorithm>
 #include <iostream>
-
 #include <set>
-
 #include <string>
-
 #include <vector>
 
-#include <algorithm>
-
-using std::string, std::cin, std::vector, std::set, std::pair, std::cout;
-
-string ReadLine() {
-    string s;
-    getline(cin, s);
+std::string ReadLine()
+{
+    std::string s;
+    std::getline(std::cin, s);
     return s;
 }
 
-int ReadLineWithNumber() {
+int ReadLineWithNumber()
+{
     int result = 0;
-    cin >> result;
+    std::cin >> result;
     ReadLine();
     return result;
 }
 
-vector < string > SplitIntoWords(const string & text) {
-    vector < string > words;
-    string word;
-    for (const char c: text) {
+std::vector<std::string> SplitIntoWords(const std::string& text)
+{
+    std::vector<std::string> words;
+    std::string word;
+    for (const char c : text) {
         if (c == ' ') {
             if (!word.empty()) {
                 words.push_back(word);
@@ -43,18 +40,20 @@ vector < string > SplitIntoWords(const string & text) {
     return words;
 }
 
-[[maybe_unused]] set < string > ParseStopWords(const string & text) {
-    set < string > stop_words;
-    for (const string & word: SplitIntoWords(text)) {
+[[maybe_unused]] std::set<std::string> ParseStopWords(const std::string& text)
+{
+    std::set<std::string> stop_words;
+    for (const std::string& word : SplitIntoWords(text)) {
         stop_words.insert(word);
     }
     return stop_words;
 }
 
-vector < string > SplitIntoWordsWithoutStopWords(const string & text,
-                                                           const set < string > & stop_words) {
-    vector < string > words;
-    for (const string & word: SplitIntoWords(text)) {
+std::vector<std::string> SplitIntoWordsWithoutStopWords(const std::string& text,
+    const std::set<std::string>& stop_words)
+{
+    std::vector<std::string> words;
+    for (const std::string& word : SplitIntoWords(text)) {
         if (stop_words.count(word) == 0) {
             words.push_back(word);
         }
@@ -62,31 +61,29 @@ vector < string > SplitIntoWordsWithoutStopWords(const string & text,
     return words;
 }
 
-void AddDocument(vector < pair < int, vector < string >>> & documents,
-                 const set < string > & stop_words,
-                 int document_id,
-                 const string & document) {
-    const vector < string > words = SplitIntoWordsWithoutStopWords(document, stop_words);
+void AddDocument(std::vector<std::pair<int, std::vector<std::string>>>& documents, const std::set<std::string>& stop_words, int document_id, const std::string& document)
+{
+    const std::vector<std::string> words = SplitIntoWordsWithoutStopWords(document, stop_words);
     documents.emplace_back(document_id, words);
 }
 
-set < string > ParseQuery(const string & text,
-                                    const set < string > & stop_words) {
-    set < string > query_words;
-    for (const string & word: SplitIntoWordsWithoutStopWords(text, stop_words)) {
+std::set<std::string> ParseQuery(const std::string& text, const std::set<std::string>& stop_words)
+{
+    std::set<std::string> query_words;
+    for (const std::string& word : SplitIntoWordsWithoutStopWords(text, stop_words)) {
         query_words.insert(word);
     }
 
     return query_words;
 }
 
-int MatchDocument(const pair < int, vector < string >> & content,
-                  const set < string > & query_words) {
+int MatchDocument(const std::pair<int, std::vector<std::string>>& content, const std::set<std::string>& query_words)
+{
     // this function does not match anything it returns a relevance of a request
     int relevance = 0;
-    vector < string > query = content.second;
+    std::vector<std::string> query = content.second;
 
-    for (const auto & i: query) {
+    for (const auto& i : query) {
         relevance += int(query_words.count(i));
     }
 
@@ -94,16 +91,15 @@ int MatchDocument(const pair < int, vector < string >> & content,
 }
 
 // for each found doc, returns an ID and a relevance
-vector < pair < int, int >> FindDocuments(const vector < pair < int, vector < string >>> & documents,
-                                                    const set < string > & stop_words,
-                                                    const string & query) {
+std::vector<std::pair<int, int>> FindDocuments(const std::vector<std::pair<int, std::vector<std::string>>>& documents, const std::set<std::string>& stop_words, const std::string& query)
+{
 
-    const set < string > query_words = ParseQuery(query, stop_words);
-    vector < int > matched_documents;
-    vector < pair < int, int >> id_and_relevance;
+    const std::set<std::string> query_words = ParseQuery(query, stop_words);
+    std::vector<int> matched_documents;
+    std::vector<std::pair<int, int>> id_and_relevance;
     int document_id = 0;
 
-    for (const auto & document: documents) {
+    for (const auto& document : documents) {
         int relevance = MatchDocument(document, query_words);
         if (relevance > 0) {
             id_and_relevance.emplace_back(document_id, relevance);
@@ -114,50 +110,49 @@ vector < pair < int, int >> FindDocuments(const vector < pair < int, vector < st
     return id_and_relevance;
 }
 
-int main() {
-    const set < string > stop_words {
-            "a",
-            "an",
-            "on",
-            "the",
-            "in",
-            "is",
-            "has",
-            "been",
-            "are",
-            "with",
-            "for",
-            "from",
-            "have",
-            "be",
-            "was"
+int main()
+{
+    const std::set<std::string> stop_words {
+        "a",
+        "an",
+        "on",
+        "the",
+        "in",
+        "is",
+        "has",
+        "been",
+        "are",
+        "with",
+        "for",
+        "from",
+        "have",
+        "be",
+        "was"
     }; // those are going to be ignored by the search engine
 
     // filling our Database with search results
-    cout << "How many results are in the Database?" << '\n';
-    vector < pair < int, vector < string >>> documents;
+    std::cout << "How many results are in the Database?" << '\n';
+    std::vector<std::pair<int, std::vector<std::string>>> documents;
     const int document_count = ReadLineWithNumber();
 
-
-    cout << "Fill the Database with potential results:" << '\n';
-    vector < string > original_queries;
+    std::cout << "Fill the Database with potential results:" << '\n';
+    std::vector<std::string> original_queries;
 
     for (int document_id = 0; document_id < document_count; ++document_id) {
-        string request = ReadLine();
+        std::string request = ReadLine();
         AddDocument(documents, stop_words, document_id, request);
         original_queries.push_back(request);
     }
 
-
-    cout << "Input your request:" << '\n';
-    const string query = ReadLine();
+    std::cout << "Input your request:" << '\n';
+    const std::string query = ReadLine();
 
     // printing results
-    cout << "You might be searching for: " << '\n';
-    for (const auto &[document_id, relevance]: FindDocuments(documents, stop_words, query)) {
-        cout << original_queries[document_id] << " : ";
-        cout << "{ relevance = " << relevance << ", document_id = " << document_id << " }" << '\n';
-    } 
+    std::cout << "You might be searching for: " << '\n';
+    for (const auto& item : FindDocuments(documents, stop_words, query)) {
+        std::cout << original_queries[item.first] << " : ";
+        std::cout << "{ relevance = " << item.second << ", document_id = " << item.first << " }" << '\n';
+    }
 
     return 0;
 }
